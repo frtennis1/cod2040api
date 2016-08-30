7466b42fb471938fa2b09ac52a007d1765ef07fe
frtennis1 <frtennis1@gmail.com>
2016-08-30 17:22:00 -0400

    finished step 2

--- 02reverse.py
+++ 02reverse.py
@@ -1,1 +1,12 @@
+import requests
+import api_tools
 
+url = api_tools.base_url + 'reverse'
+data = {'token' : api_tools.token}
+
+r = requests.post(url, data)
+
+data.update({'string': r.text[::-1]})
+validate_url = url + '/validate'
+
+r_validate = requests.post(validate_url, data)
